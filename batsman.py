e3c9d0001a4ddef565a58de34356f6fd30d3ca3e
kshitijakarsh <kshitijakarsh@gmail.com>
2024-01-14 18:23:06 +0530

    Added Bar Graph

--- batsman.py
+++
@@ -1,9 +0,0 @@
-batsman_team_1 = []
-def batsman(team_1_players):
-    batsman_team_1 = team_1_players.copy()
-    batsman_team_1.pop(10)
-    batsman_team_1.pop(9)
-    batsman_team_1.pop(8)
-    batsman_team_1.pop(7)
-    batsman_team_1.pop(6)
-    print(batsman_team_1)
